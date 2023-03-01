6d0996c806472aeb05f1487bc431d90f2322cd71
Wilis Jean <wilisjean@hotmail.com>
2023-02-28 21:04:22 -0300

    atualizações do site 28-02-2023
    atualizações do site 28-02-2023

--- script.js
+++ script.js
@@ -35,4 +35,24 @@ $(document).ready(function(){
 
   });
 
+  //FUNÇÃO QUE RECEBE UMA DATA DE NASCIMENTO E CALCULA  A IDADE ano atual menos ano de nascimento
+  function getAge(dateString) {
+    const today = new Date();
+    const birthDate = new Date(dateString);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+        
+    //se o mês atual for menor que o mês de aniversário, diminui 1 ano
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    
+    return age;
+  }
+
+  //variavel que chama a função e recebe a idade
+  let idade = getAge('1990/06/12')
+  //seleciona  a classe que vai receber o valor da idade na página html
+  $('.idade').html(idade)
+  
 });
